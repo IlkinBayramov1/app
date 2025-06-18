0df1061cc9fd8824ac748f377977efecdfb58c4c
John Doe <johndoe@example.com>
2025-06-18 12:14:00 +0400

    Yeni dəyişikliklər əlavə edildi

--- back/server.js
+++ back/server.js
@@ -9,14 +9,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api', hotelRoutes);
 
-// MongoDB bağlantısını çağır
 connectDB();
 
 app.listen(PORT, () => {
---
+++ front/src/pages/home/components/sectwo/Sectwo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sectwo = () => {
+    return (
+        <div>Sectwo</div>
+    )
+}
+
+export default Sectwo
